629ea2b64a0d91aed2100007e9668cb636cb9fc1
Newton <newton.bundi@student.moringaschool.com>
2023-07-13 21:22:28 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
-// Write your code here
+const num1 = 31;
+const num2 = 2;
+const num3 = 12;
+const num4 = 8;
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random())+1;
+
+const mod = num3 % num4;
+
+const max = Math.max(20,2,11,17,5,0)
+
+console.log(multiply, random, mod,max)
